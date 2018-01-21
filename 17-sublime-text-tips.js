ddc24efd52490ef9e62b30957ad35c22bf169364
michaelcheng429 <cheng@csdisco.com>
2018-01-20 19:32:05 -0600

    initial commit

---
+++ 17-sublime-text-tips.js
@@ -0,0 +1,79 @@
+// 17 Sublime Text tips and shortcuts to save you tons of time
+
+// Preface) Get project started
+
+// 1) CMD+CLICK (CTRL+CLICK) --> Multi-cursor
+
+// 2) CMD+D (CTRL+D) --> Select next occurrence
+
+// 3) CMD+K (CTRL+K) --> Skip occurrence
+
+var something = 1;
+something = 2;
+something = 3;
+
+// 4) CMD+P (CTRL+P) --> Jump to file
+
+// 5) CMD+ALT+NUMBER (CTRL+ALT+NUMBER) --> Multiple column layout
+
+// 6) CMD+SHIFT+F (CTRL+SHIFT+F) --> Search all files
+
+// 7) Key binding for "console.log();"
+
+// 8) CMD+SHIFT+SPACE (CTRL+SHIFT+SPACE) --> Select entire string
+
+'a string';
+
+// 9) Package Control (packagecontrol.io/installation)
+
+// 10) SidebarEnhancements
+
+// 11) CMD+X (CTRL+X) --> Delete/Cut entire line
+
+var sampleLine = 'sample line';
+
+// 12) CMD+CTRL+UP/DOWN (CTRL+SHIFT+UP/DOWN) --> Move line/lines
+
+var something = [
+    {
+        key1: 1
+    },
+    {
+        key2: 2,
+        key3: 3
+    }
+];
+
+// 13) CMD+SHIFT+V (CTRL+SHIFT+V) --> Paste with proper formatting
+
+var something = [
+    {
+        key1: 1
+    },
+    {
+        key2: 2,
+        key3: {
+            key4: 4
+        }
+    }
+];
+
+var arr = [
+
+];
+
+// 14) CMD+BRACKET (CTRL+BRACKET) --> Indent / De-indent
+
+var testLine = 'test line';
+
+// 15) CMD+L (CTRL+L) --> Select entire line
+
+var lineToSelect = 'line to select';
+
+// 16) Word Wrap
+
+var message = 'this is a test message that is so long that it extends past just one single line';
+
+// 17) CMD+SHIFT+M (CTRL+SHIFT+M) --> Select entire contents of parentheses, brackets, or braces
+
+var arr = ['item1', 'item2']
